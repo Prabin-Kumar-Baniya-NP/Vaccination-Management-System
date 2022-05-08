e2c0171061e10f93011ce2f939075d78b79e7d49
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2022-05-08 15:35:59 +0530

    static configuration

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
          namespace="medical_condition")),
     path("vaccine/", include("vaccine.urls", namespace="vaccine")),
     path("vaccination/", include("vaccination.urls", namespace="vaccination")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
