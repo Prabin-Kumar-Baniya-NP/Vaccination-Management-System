4b0661a02c62dd61267cab3714e636a7e2737364
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2022-05-09 01:44:40 +0530

    bootstrap added to vaccine templates

--- mysite/vaccine/forms.py
+++ mysite/vaccine/forms.py
@@ -3,12 +3,22 @@ from vaccine.models import Vaccine
 
 
 class VaccineCreateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(VaccineCreateForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Vaccine
         fields = "__all__"
 
 
 class VaccineUpdateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(VaccineUpdateForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Vaccine
         fields = "__all__"
