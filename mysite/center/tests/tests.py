685c9c7a12e20e6ca6c76cf756fb4b8f2d089b78
Prabin Kumar Baniya <pra44bin@gmail.com>
2022-07-16 15:31:35 +0545

    Test case code refactor

--- mysite/center/tests/tests.py
+++ mysite/center/tests/tests.py
@@ -14,67 +14,66 @@ fake = Faker()
 class TestPermissionsOnCenterView(TestCase):
     def setUp(self):
         self.c = Client()
-        self.center1 = CenterFactory()
-        self.storage1 = StorageFactory()
+        self.center = CenterFactory()
         self.user = AdminUserFactory()
         self.c.login(email=self.user.email, password="abcde@12345")
         return super().setUp()
 
-    def test_user_can_access_create_center_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(reverse("center:create-center"))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+    def test_unauthorized_access_on_create_center_view(self):
+        response = self.c.get(reverse("center:create-center"))
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_create_center_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="add_center"))
-        response2 = self.c.get(reverse("center:create-center"))
-        self.assertEqual(response2.status_code, 200)
+        response = self.c.get(reverse("center:create-center"))
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_update_center_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
-            reverse("center:center-update", kwargs={"pk": self.center1.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+    def test_unauthorized_access_on_update_center_view(self):
+        response = self.c.get(
+            reverse("center:center-update", kwargs={"pk": self.center.id}))
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_update_center_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="change_center"))
-        response2 = self.c.get(
-            reverse("center:center-update", kwargs={"pk": self.center1.id}))
-        self.assertEqual(response2.status_code, 200)
+        response = self.c.get(
+            reverse("center:center-update", kwargs={"pk": self.center.id}))
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_center_list_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(reverse("center:center-list"))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+    def test_unauthorized_access_on_center_list_view(self):
+        response = self.c.get(reverse("center:center-list"))
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_center_list_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="view_center"))
-        response2 = self.c.get(reverse("center:center-list"))
-        self.assertEqual(response2.status_code, 200)
+        response = self.c.get(reverse("center:center-list"))
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_center_detail_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
-            reverse("center:center-detail", kwargs={"pk": self.center1.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+    def test_unauthorized_access_on_center_detail_view(self):
+        response = self.c.get(
+            reverse("center:center-detail", kwargs={"pk": self.center.id}))
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_center_detail_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="view_center"))
-        response2 = self.c.get(
-            reverse("center:center-detail", kwargs={"pk": self.center1.id}))
-        self.assertEqual(response2.status_code, 200)
+        response = self.c.get(
+            reverse("center:center-detail", kwargs={"pk": self.center.id}))
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_delete_center_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
-            reverse("center:center-delete", kwargs={"pk": self.center1.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+    def test_unauthorized_access_on_delete_center_view(self):
+        response = self.c.get(
+            reverse("center:center-delete", kwargs={"pk": self.center.id}))
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_delete_center_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="delete_center"))
-        response2 = self.c.get(
-            reverse("center:center-delete", kwargs={"pk": self.center1.id}))
-        self.assertEqual(response2.status_code, 200)
+        response = self.c.get(
+            reverse("center:center-delete", kwargs={"pk": self.center.id}))
+        self.assertEqual(response.status_code, 200)
 
 
 class TestCenterView(TestCase):
@@ -175,65 +174,65 @@ class TestPermissionsOnStorageView(TestCase):
         self.c.login(email=self.user.email, password="abcde@12345")
         return super().setUp()
 
-    def test_user_can_access_create_storage_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
+    def test_unauthorized_access_on_create_storage_view(self):
+        response = self.c.get(
             reverse("center:storage-create", kwargs={"center_id": self.storage.center.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_create_storage_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="add_storage"))
-        response2 = self.c.get(
+        response = self.c.get(
             reverse("center:storage-create", kwargs={"center_id": self.storage.center.id}))
-        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_update_storage_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
+    def test_unauthorized_access_on_update_storage_view(self):
+        response = self.c.get(
             reverse("center:storage-update", kwargs={"pk": self.storage.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_update_storage_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="change_storage"))
-        response2 = self.c.get(
+        response = self.c.get(
             reverse("center:storage-update", kwargs={"pk": self.storage.id}))
-        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_storage_list_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
+    def test_unauthorized_access_on_storage_list_view(self):
+        response = self.c.get(
             reverse("center:storage-list", kwargs={"center_id": self.storage.center.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_storage_list_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="view_storage"))
-        response2 = self.c.get(
+        response = self.c.get(
             reverse("center:storage-list", kwargs={"center_id": self.storage.center.id}))
-        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_storage_detail_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
+    def test_unauthorized_access_on_storage_detail_view(self):
+        response = self.c.get(
             reverse("center:storage-detail", kwargs={"pk": self.storage.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_storage_detail_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="view_storage"))
-        response2 = self.c.get(
+        response = self.c.get(
             reverse("center:storage-detail", kwargs={"pk": self.storage.id}))
-        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_can_access_delete_storage_view(self):
-        # Check unauthorized access
-        response1 = self.c.get(
+    def test_unauthorized_access_on_delete_storage_view(self):
+        response = self.c.get(
             reverse("center:storage-delete", kwargs={"pk": self.storage.id}))
-        self.assertEqual(response1.status_code, 403)
-        # Check authorized access
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access_on_delete_storage_view(self):
         self.user.user_permissions.add(
             Permission.objects.get(codename="delete_storage"))
-        response2 = self.c.get(
+        response = self.c.get(
             reverse("center:storage-delete", kwargs={"pk": self.storage.id}))
-        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
 
 class TestStorageView(TestCase):
