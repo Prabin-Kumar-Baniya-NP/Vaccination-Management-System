bb631b9ae9163d2e2a73030edb2907deacf3ebc0
Prabin Kumar Baniya <pra44bin@gmail.com>
2022-08-13 23:34:55 +0530

    Removed test cases for vaccination list for patient view

--- mysite/vaccination/tests/test_permissions.py
+++ mysite/vaccination/tests/test_permissions.py
@@ -25,16 +25,6 @@ class TestPermissionsOnVaccinationView(TestCase):
                               kwargs={"campaign_id": self.vaccination.campaign.id}))
         self.assertEqual(response.status_code, 200)
 
-    def test_unauthorized_access_on_vaccination_list_view_for_patient(self):
-        response = self.c.get(reverse("vaccination:vaccination-list-patient"))
-        self.assertEqual(response.status_code, 403)
-
-    def test_authorized_access_on_vaccination_list_view_for_patient(self):
-        self.user.user_permissions.add(
-            Permission.objects.get(codename="view_vaccination"))
-        response = self.c.get(reverse("vaccination:vaccination-list-patient"))
-        self.assertEqual(response.status_code, 200)
-
     def test_unauthorized_access_on_vaccination_detail_view(self):
         response = self.c.get(reverse("vaccination:vaccination-detail",
                               kwargs={"pk": self.vaccination.id}))
