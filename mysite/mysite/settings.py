de3252aeed20b30fa625db1f5509c4af2f5eb901
Prabin Kumar Baniya <pra44bin@gmail.com>
2023-07-30 15:28:07 +0530

    FIx debug booleand value and added gunicorn in req.txt file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,9 +14,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
