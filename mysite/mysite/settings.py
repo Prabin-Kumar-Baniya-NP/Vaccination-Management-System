a965c6d048662c3fe7024dd05fe7be9a21547602
Prabin Kumar Baniya <pra44bin@gmail.com>
2024-03-19 01:50:36 +0530

    Configured to allow all host in settings.py file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 DEBUG_PROPAGATE_EXCEPTIONS = True # To handle exceptions manually
 
